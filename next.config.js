9f694b1926d1fe74ffc312b38a1f682a3b59821a
alfset <alfset5@gmail.com>
2025-04-23 04:30:42 +0700

    add iniitially repo

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
-// next.config.js ✅
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+module.exports = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
-
-module.exports = nextConfig;
